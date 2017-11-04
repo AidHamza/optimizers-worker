bd2e50791acd4ea498605df8bbbebfe176e18166
Hamza Aid <hamza@appsdoc.com>
2017-11-04 10:03:52 +0000

    Use Protobuf operation in cmd

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -1,38 +1,57 @@
 package command
 
 import (
+	"bytes"
 	"context"
-	"fmt"
 	"time"
+	"strings"
 	"os/exec"
+
+	"github.com/AidHamza/optimizers-worker/pkg/log"
 )
 
 const COMMAND_TIMEOUT time.Duration = 10
 
 type Handler interface {
-	RunCommand(string, []string)
+	RunCommand(string, []string, []byte) ([]byte, error)
+	EnableDebug(bool)
 }
 
 type handler struct {
 	command *exec.Cmd
 	ctx context.Context
 	cancel context.CancelFunc
+	debug bool
 }
 
-func(h *handler) RunCommand(cmd string, args []string) {
+func(h *handler) RunCommand(cmd string, args []string, imgBuf []byte) ([]byte, error) {
+	var cmdOut bytes.Buffer
+
 	h.ctx, h.cancel = context.WithTimeout(context.Background(), COMMAND_TIMEOUT * time.Second)
 	defer h.cancel()
 	h.command = exec.CommandContext(h.ctx, cmd, args...)
+	h.command.Stdin = strings.NewReader(string(imgBuf))
+	h.command.Stdout = &cmdOut
 
-	out, err := h.command.Output()
+	if h.debug == true {
+		log.Logger.Error("Executing", "CMD", strings.Join(h.command.Args, " "))
+	}
+
+	if err := h.command.Run(); err != nil {
+        log.Logger.Error("Cannot start optimizers command, Non-zero exit code", "CMD_START_FAILED", err.Error(), "CMD", cmd, "ARGS", args)
+        return []byte{0}, err
+    }
+
 	if h.ctx.Err() == context.DeadlineExceeded {
-		fmt.Println("Command timed out")
+		log.Logger.Error("Command Timeout reached", "CMD_TIMEOUT_REACHED", h.ctx.Err(), "CMD", cmd, "ARGS", args)
+		return []byte{0}, h.ctx.Err()
 	}
 
-	fmt.Println("Output:", string(out))
-	if err != nil {
-		fmt.Println("Non-zero exit code:", err)
-	}
+	return cmdOut.Bytes(), nil
+}
+
+func(h *handler) EnableDebug(isDebug bool) {
+	h.debug = isDebug
 }
 
 func NewHandler() Handler {
